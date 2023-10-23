c2ded0947aa42a021a43b61199a1ec7fc471f2eb
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-10-23 21:15:45 +0800

    ESP_Lamp

---
+++ mqtt/Config.h
@@ -0,0 +1,9 @@
+String AP_NAME = "Krushiler_Network_";
+String AP_PASSWORD = "13371488";
+
+
+const char* mqtt_broker = "broker.emqx.io";
+const int mqtt_port = 1883;
+String topic = "esp8266_Krushiler/command";
+
+int WEB_SERVER_PORT = 80;
